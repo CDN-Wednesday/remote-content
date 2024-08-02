017ffe83f27bc80181772cc7110252b6e1576c88
Wednesday Addams <jordanluders@proton.me>
2024-08-02 18:39:37 -0500

    Update team-unusual-logo.js

--- 2419f29c45db4eb46245c8b89cf34dce35f6db8fb40a3631fd5424cb727b1ce3/assets/js/2048-Clones/team-unusual-logo.js
+++ 2419f29c45db4eb46245c8b89cf34dce35f6db8fb40a3631fd5424cb727b1ce3/assets/js/2048-Clones/team-unusual-logo.js
@@ -1,1 +1,1 @@
-document.getElementById("team-unusual-logo").textContent="https://cdn-r5-3600.github.io/remote-content/2419f29c45db4eb46245c8b89cf34dce35f6db8fb40a3631fd5424cb727b1ce3/assets/images/Team-Unusual/icon2.png";
+document.getElementById("team-unusual-logo").textContent="https://cdn-wednesday.github.io/remote-content/2419f29c45db4eb46245c8b89cf34dce35f6db8fb40a3631fd5424cb727b1ce3/assets/images/Team-Unusual/icon2.png";
